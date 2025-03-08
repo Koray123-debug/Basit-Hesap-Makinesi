d5bcd7c8a540226421d2196c143c478e090a55c7
Koray123-debug <razoruposas@icloud.com>
2025-03-08 00:41:02 -0800

    Add files via upload

---
+++ Generator.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Basit_Hesap_Makinesi
+{
+    class Generator
+    {
+        string IO = "";
+        string API = "";
+    }
+}
